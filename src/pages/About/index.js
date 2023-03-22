532d7869ed385093dd9680dcded19db28fc996cb
agtTwilight <lukasmacmillen@gmail.com>
2023-03-22 12:47:33 -0700

    changed footer positioning

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -6,10 +6,10 @@ export const About = () => {
     <div className='wrap'>
       <p className='pistachio'>{"Hello, my name is"}</p>
       <h1>{"Lukas MacMillen"}</h1>
-      <h1>{"I Develop Interactive UX & Their Databases"}</h1>
-      <p>{"I am a Seattle-based software developer open to work with any layer of the tech stack. I prefer working in teams following an agile development process, and my ideal position would have me working as a full-stack developer with an emphasis on the back-end. Currently I am most profecient with JavaScript (inlcuding React.js & Node.js), MongoDb, and mySQL databases, but I have previous experience with MATLAB, Python and R, and I am confident in my abilities to learn new tools and contribute to any team!"}
+      <h2>{"I build interactive user experiences"}</h2>
+      <p className='aboutP'>I am a Seattle-based software developer specializing in building user experiences and databases. I'm currently focused on the MERN stack, but have previous experience with MySQL, MATLAB, Python, and R. <u>I am open to work</u> in any layer of the tech stack, but would prefer a team rich with mentorship and following agile development practices.
       </p>
-      <a id='portfolio-anchor' href='/projects'>{"See my work -->"}</a>
+      <a id='portfolio-anchor' href='/portfolio'>See my work →</a>
     </div>
   )
 }
