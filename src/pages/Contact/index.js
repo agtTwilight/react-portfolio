cb31f7a68d3c89d8a9f10680049da040926405f0
agtTwilight <lukasmacmillen@gmail.com>
2023-03-23 10:54:52 -0700

    added mobile featuers to my contact form

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,12 +3,13 @@ import "./style.css"
 import ContactForm from "../../components/ContactForm"
 
 export const Contact = () => {
-  return (
-    <section id='contactContainer'>
-        <p id='contactSub'>Let's Connect!</p>
-        <ContactForm/>
-    </section>
-  )
+    return (
+        <section id='contactContainer'>
+            <p>I'm currently <strong>open to work!</strong></p>
+            <ContactForm />
+            <p>Happy to chat about how I can contribute to your team as a backend or fullstack developer. Should you have any questions or just want to chat, my inbox is always open 😸</p>
+        </section>
+    )
 }
 
 export default Contact
